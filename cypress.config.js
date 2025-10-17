f8f8e67289ffd1f14b295a97137991397116124d
Abimanyu Mani <m.abimanyu@gmail.com>
2025-10-17 13:32:45 +1100

    cypress-config-uddated

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,9 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/tests/**/*.cy.js',
     baseUrl: 'http://localhost:3000', // optional
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
   // reporter: 'cypress-mochawesome-reporter',
   // reporterOptions: {
